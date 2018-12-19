d0307d915f715aeddbcd8f0ca66b5265684f2c91
unknown <limteikyee@gmail.com>
2018-12-20 02:06:55 +0800

    small update

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import server from './server'
 /* eslint-disable no-alert, no-console */
 server.start(
   {
-    cors: { origin: 'https://dinewithme.app/*' },
+    //cors: { origin: 'https://dinewithme.app/*' },
     port: process.env.NODE_PORT || 4000,
   },
   () => {
