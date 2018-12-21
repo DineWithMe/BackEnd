c0d4d8f2304fe4e9e921fb22e32b13c99f023646
unknown <limteikyee@gmail.com>
2018-12-21 23:28:57 +0800

    add some rest api

---
+++ src/controller/mailChimp.js
@@ -0,0 +1,5 @@
+const handleMailChimp = (req, res) => {
+  return res.status(200).json('success')
+}
+
+export { handleMailChimp }
--- src/server.js
+++ src/server.js
@@ -1,5 +1,8 @@
 import { GraphQLServer, PubSub } from 'graphql-yoga'
 import { resolvers, fragmentReplacements } from './resolvers/index'
+import { handleMailChimp } from './controller/mailChimp'
+import cors from 'cors'
+import bodyParser from 'body-parser'
 import prisma from './prismaBinding'
 
 const pubsub = new PubSub()
@@ -15,6 +18,17 @@ const server = new GraphQLServer({
     }
   },
   fragmentReplacements,
+})
+
+server.express.use(cors())
+
+server.express.use(bodyParser.urlencoded())
+
+server.express.get('/mailchimp', (req, res) => {
+  handleMailChimp(req, res)
+})
+server.express.post('/mailchimp', (req, res) => {
+  res.json('success')
 })
 
 export { server as default }
