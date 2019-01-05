bde1588fb8d519eae1fa218caba94957c33b9631
unknown <limteikyee@gmail.com>
2019-01-05 23:45:36 +0800

    check user existence before assigning new token

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -72,9 +72,13 @@ const Query = {
       info
     )
   },
-  async verifyToken(parent, args, { request }, info) {
+  async verifyToken(parent, args, { prisma, request }) {
     const decoded = getDecodedToken(request)
     let { userToken } = decoded
+
+    if (!(await prisma.exists.User({ username: decoded.username }))) {
+      throwError(6001, 'user not exist')
+    }
     // refresh token if less then 7 days
     if (decoded.exp - moment().format('X') < 86400 * 7) {
       delete decoded.userToken
