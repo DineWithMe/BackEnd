02b9e85c56c0b2051e481a3cc72d37b957ca9934
unknown <limteikyee@gmail.com>
2019-01-07 18:42:32 +0800

    switch from bcrypt python to bcrpyt js to make life easier

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,4 +1,4 @@
-import bcrypt from 'bcrypt'
+import bcrypt from 'bcryptjs'
 import getDecodedToken from '../utils/getDecodedToken'
 import generateToken from '../utils/generateToken'
 import hashPassword from '../utils/hashPassword'
