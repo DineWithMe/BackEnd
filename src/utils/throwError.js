d3ade87552f4b1ccf7b6c854e37daf220b115595
unknown <limteikyee@gmail.com>
2019-01-08 18:57:40 +0800

    add flexibility

--- src/utils/throwError.js
+++ src/utils/throwError.js
@@ -1,7 +1,11 @@
 const throwError = (statusCode, error, message) => {
   // https://stackoverflow.com/questions/50954538/javascript-can-you-throw-an-object-in-an-error
   throw new Error(
-    JSON.stringify({ statusCode, message: message || error.message, error })
+    JSON.stringify({
+      statusCode,
+      message: message || error.message || error,
+      error,
+    })
   )
 }
 
