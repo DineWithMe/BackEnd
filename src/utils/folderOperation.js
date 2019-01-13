145e3187da3c990f5ab8eedb1bb194a361ff9c72
unknown <limteikyee@gmail.com>
2019-01-13 10:41:40 +0800

    will not check condition if user is null

--- src/utils/folderOperation.js
+++ src/utils/folderOperation.js
@@ -2,7 +2,7 @@ import { USER_AVATAR } from '../constants/folder'
 import { existsSync } from 'fs'
 
 const avatarExist = (user) => {
-  if (!existsSync(`${USER_AVATAR}${user.avatarFilename}`)) {
+  if (user && !existsSync(`${USER_AVATAR}${user.avatarFilename}`)) {
     // if no avatar found null the name
     user.avatarFilename = null
   }
